71775d6a6502ea72e748452e26c7abee15d2ed32
Baxa <bahbergen998@gmail.com>
2026-07-21 20:20:08 +0600

    Fix db.js export to ES module

--- config/db.js
+++ config/db.js
@@ -13,10 +13,33 @@
 
 // module.exports = pool;
 
-const { Pool } = require("pg");
-require("dotenv").config(); // .env файлын оқу үшін
+//////////////////////////////////////////////////////////////////////////////////////////
 
-// Eгер process.env.DATABASE_URL болмаса, локальный URL-ді ала салады
+// const { Pool } = require("pg");
+// require("dotenv").config(); // .env файлын оқу үшін
+
+// // Eгер process.env.DATABASE_URL болмаса, локальный URL-ді ала салады
+// const connectionString =
+//   process.env.DATABASE_URL ||
+//   "postgresql://sport_booking:PCNZSJ7G4tBJqmqJvLU2Bg1RIsX9JyxK@dpg-d9f6a1t7vvec73fnomh0-a.oregon-postgres.render.com/sport_booking_w5jf";
+
+// const pool = new Pool({
+//   connectionString: connectionString,
+//   ssl: {
+//     rejectUnauthorized: false, // Render Postgres үшін міндетті
+//   },
+// });
+
+// module.exports = pool;
+
+////////////////////////////////////////////////////////////////////////////////////////////
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pg;
+
 const connectionString =
   process.env.DATABASE_URL ||
   "postgresql://sport_booking:PCNZSJ7G4tBJqmqJvLU2Bg1RIsX9JyxK@dpg-d9f6a1t7vvec73fnomh0-a.oregon-postgres.render.com/sport_booking_w5jf";
@@ -24,8 +47,8 @@ const connectionString =
 const pool = new Pool({
   connectionString: connectionString,
   ssl: {
-    rejectUnauthorized: false, // Render Postgres үшін міндетті
+    rejectUnauthorized: false,
   },
 });
 
-module.exports = pool;
+export default pool; // <-- Рекса, дәл осылай export default жасаймыз!
